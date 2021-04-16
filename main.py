9200fa3cb80fb802bb11b4a5782b9d7ad5c64e15
Joshua <69980110+4CorneredCircle@users.noreply.github.com>
2021-04-15 20:20:40 -0500

    New update with slow mode and hazards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 import os
+import random
+
+from pygame.constants import MOUSEBUTTONDOWN
 pygame.font.init()
 pygame.mixer.init()
 pygame.init()
@@ -28,6 +31,8 @@ SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 YELLOW_HIT = pygame.USEREVENT + 1
 RED_HIT = pygame.USEREVENT + 2
 
+METEOR_HAZARD_EVENT = pygame.USEREVENT + 3
+
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
 YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
@@ -36,9 +41,20 @@ RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMA
 
 SPACE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'space.png')), (WIDTH, HEIGHT))
 
-def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health, red_ship_type, yellow_ship_type):
+MAIN_MENU_BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'background.png')), (WIDTH, HEIGHT))
+
+METEOR_WIDTH, METEOR_HEIGHT = 64, 64
+METEOR_VEL = 3
+
+METEOR_IMAGE = pygame.image.load(os.path.join('Assets', 'flaming_meteor.png'))
+METEOR = pygame.transform.scale(METEOR_IMAGE, (METEOR_WIDTH, METEOR_HEIGHT))
+
+def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health, red_ship_type, yellow_ship_type, meteor_list):
     WIN.blit(SPACE, (0, 0))
     pygame.draw.rect(WIN, BLACK, BORDER)
+
+    for meteor in meteor_list:
+        WIN.blit(METEOR, (meteor.x, meteor.y))
 
     red_health_text = HEALTH_FONT.render("Health: " + str(red_health), 1, WHITE)
     yellow_health_text = HEALTH_FONT.render("Health: " + str(yellow_health), 1, WHITE)
@@ -102,11 +118,27 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
         elif bullet.x < 0:
             red_bullets.remove(bullet)
 
+def handle_meteors(meteor_list, yellow, red):
+    for meteor in meteor_list:
+        meteor.y += METEOR_VEL
+        if red.colliderect(meteor):
+            pygame.event.post(pygame.event.Event(RED_HIT))
+            meteor_list.remove(meteor)
+        if yellow.colliderect(meteor):
+            pygame.event.post(pygame.event.Event(YELLOW_HIT))
+            meteor_list.remove(meteor)
+        elif meteor.y > HEIGHT:
+            meteor_list.remove(meteor)
+
 def draw_winner(text):
     draw_text = WINNER_FONT.render(text, 1, WHITE)
     WIN.blit(draw_text, (WIDTH//2 - draw_text.get_width()//2, HEIGHT//2 - draw_text.get_height()//2))
     pygame.display.update()
     pygame.time.delay(5000)
+    main_menu()
+
+def hazard_function():
+    pygame.time.set_timer(METEOR_HAZARD_EVENT, 15000)
 
 def main():
     red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -127,6 +159,10 @@ def main():
     red_health = 10
     yellow_health = 10
 
+    meteor_list = []
+
+    counter = 0
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -137,6 +173,7 @@ def main():
                 pygame.quit()
 
             if event.type == pygame.KEYDOWN:
+                # firing bullets
                 if event.key == pygame.K_LCTRL and len(yellow_bullets) < yellow_max_bullets:
                     bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
                     yellow_bullets.append(bullet)
@@ -148,22 +185,22 @@ def main():
                     BULLET_FIRE_SOUND.play()
 
                 #handles switching ship types
-                if event.key == pygame.K_TAB and yellow_ship_type == 1:
+                if event.key == pygame.K_LSHIFT and yellow_ship_type == 1:
                     yellow_vel = 3
                     yellow_max_bullets = 6
                     yellow_ship_type = 2
 
-                elif event.key == pygame.K_TAB and yellow_ship_type ==2:
+                elif event.key == pygame.K_LSHIFT and yellow_ship_type ==2:
                     yellow_vel = 5
                     yellow_max_bullets = 3
                     yellow_ship_type = 1
 
-                if event.key == pygame.K_HOME and red_ship_type == 1:
+                if event.key == pygame.K_RSHIFT and red_ship_type == 1:
                     red_vel = 3
                     red_max_bullets = 6
                     red_ship_type = 2
 
-                elif event.key == pygame.K_HOME and red_ship_type == 2:
+                elif event.key == pygame.K_RSHIFT and red_ship_type == 2:
                     red_vel = 5
                     red_max_bullets = 3
                     red_ship_type = 1
@@ -175,6 +212,13 @@ def main():
             if event.type == YELLOW_HIT:
                 yellow_health -= 1
                 BULLET_HIT_SOUND.play()
+
+            if event.type == METEOR_HAZARD_EVENT:
+                while counter < random.randint(2, 4):
+                    meteor_rect = pygame.Rect(random.randrange(1, WIDTH - METEOR_WIDTH, 10), 0, METEOR_WIDTH, METEOR_HEIGHT)
+                    meteor_list.append(meteor_rect)
+                    counter += 1
+                counter = 0
 
         winner_text = ""
         if red_health <= 0:
@@ -193,9 +237,70 @@ def main():
 
         handle_bullets(yellow_bullets, red_bullets, yellow, red)
 
-        draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health, red_ship_type, yellow_ship_type)
+        handle_meteors(meteor_list, yellow, red)
+
+        draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health, red_ship_type, yellow_ship_type, meteor_list)
     
     main()
 
+def main_menu():
+    hazards = False
+
+    click = False
+
+    menu_open = True
+    while menu_open:
+        WIN.blit(MAIN_MENU_BACKGROUND, (0, 0))
+
+        title_background = pygame.Rect(295, 25, 310, 100)
+        pygame.draw.rect(WIN, BLACK, title_background)
+
+        title_text = HEALTH_FONT.render("SPACESHIPS", 1, WHITE)
+        subheading_text = HEALTH_FONT.render("Choose your options:", 1, WHITE)
+        WIN.blit(title_text, (WIDTH//2 - title_text.get_width()//2, HEIGHT//2 - title_text.get_height()//2 - 200))
+        WIN.blit(subheading_text, (WIDTH//2 - subheading_text.get_width()//2, HEIGHT//2 - subheading_text.get_height()//2 - 150))
+
+        mx, my = pygame.mouse.get_pos()
+
+        hazard_button = pygame.Rect(350, 200, 200, 50)
+        no_hazard_button = pygame.Rect(350, 300, 200, 50)
+        start_button = pygame.Rect(350, 400, 200, 50)
+        #check to see if the mouse has collided with the button and if the mouse is also being clicked
+        if hazard_button.collidepoint((mx, my)):
+            if click:
+                hazards = True
+        if no_hazard_button.collidepoint((mx, my)):
+            if click:
+                hazards = False
+        if start_button.collidepoint((mx, my)):
+            if click:
+                if hazards == True:
+                    hazard_function()
+                    main()
+                elif hazards == False:
+                    main()
+        pygame.draw.rect(WIN, BLACK, hazard_button)
+        pygame.draw.rect(WIN, BLACK, no_hazard_button)
+        pygame.draw.rect(WIN, BLACK, start_button)
+
+        hazard_text = HEALTH_FONT.render("Hazards on", 1, WHITE)
+        no_hazard_text = HEALTH_FONT.render("Hazards off", 1, WHITE)
+        start_text = HEALTH_FONT.render("Start", 1, WHITE)
+        WIN.blit(hazard_text, (hazard_button.x + 20, hazard_button.y + 10))
+        WIN.blit(no_hazard_text, (no_hazard_button.x + 20, no_hazard_button.y + 10))
+        WIN.blit(start_text, (start_button.x + 65, start_button.y + 10))
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pygame.display.update()
+        clock = pygame.time.Clock()
+        clock.tick(FPS)
+
 if __name__ == "__main__":
-    main()
+    main_menu()
